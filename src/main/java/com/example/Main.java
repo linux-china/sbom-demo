39de6d60e6189fdff0516240623b81f409548a8c
linux_china <libing.chen@gmail.com>
2024-03-20 13:56:42 +0800

    chore: add javadoc

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,5 +1,10 @@
 package com.example;
 
+/**
+ * main entrance
+ *
+ * @author linux_china
+ */
 public class Main {
     public static void main(String[] args) {
         System.out.println("good morning");
