f9233b10f8b18bc8172b7629177c817425954d58
linux_china <libing.chen@gmail.com>
2024-03-22 13:59:20 +0800

    app: SpringBootDemoApp

---
+++ src/main/java/com/example/SpringBootDemoApp.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * main entrance
+ *
+ * @author linux_china
+ */
+@SpringBootApplication
+public class SpringBootDemoApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoApp.class, args);
+    }
+}
